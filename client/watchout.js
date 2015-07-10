5053cb90609bac0d181920c4145c7ecec9879a9a
Hack Reactor Students <accounts@hackreactor.com>
2015-07-10 14:44:43 -0700

    enemies added

--- client/watchout.js
+++ client/watchout.js
@@ -1,2 +1,10 @@
 // start slingin' some d3 here.
 
+var enemies = d3.select('.game').selectAll('image').data([{x: 10, y:10}, {x: 20, y: 20}, {x: 30, y: 30}]).enter().append('image');
+enemies.attr({'xlink:href': 'asteroid.png', width: '20px', height: '20px'});
+enemies.attr('x', function(pos) {
+  return pos.x;
+});
+enemies.attr('y', function(pos) {
+  return pos.y;
+});
