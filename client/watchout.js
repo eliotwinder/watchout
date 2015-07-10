a1cdceb6018dc39e989e351e1d0e74988c1f29c5
Hack Reactor Students <accounts@hackreactor.com>
2015-07-10 15:08:15 -0700

    moving roids

--- client/watchout.js
+++ client/watchout.js
@@ -1,10 +1,35 @@
 // start slingin' some d3 here.
 
-var enemies = d3.select('.game').selectAll('image').data([{x: 10, y:10}, {x: 20, y: 20}, {x: 30, y: 30}]).enter().append('image');
-enemies.attr({'xlink:href': 'asteroid.png', width: '20px', height: '20px'});
-enemies.attr('x', function(pos) {
-  return pos.x;
-});
-enemies.attr('y', function(pos) {
-  return pos.y;
-});
+var updateEnemyPosition = function(positions) {
+  var enemies = d3.select('.game').selectAll('image').data(positions)
+  
+  enemies.attr('x', function(pos) {
+    return pos.x;
+  });
+  
+  enemies.attr('y', function(pos) {
+    return pos.y;
+  });
+
+  enemies.enter().append('image').attr({'xlink:href': 'asteroid.png', width: '20px', height: '20px'}).attr('x', function(pos) {
+    return pos.x;
+  }).attr('y', function(pos) {
+    return pos.y;
+  });
+
+  enemies.exit().remove();
+};
+
+var randomPositions = function(n) {
+  var results = [];
+  var bBox = document.getElementsByClassName('game')[0].getBBox();
+  for (var i = 0; i < n; i++) {
+    results.push({x: Math.random() * bBox.width, y: Math.random() * bBox.height});
+  }
+
+  return results;
+}
+
+setInterval(function() {
+  updateEnemyPosition(randomPositions(3));
+}, 1000);
