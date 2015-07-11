7d777443d756c8e4cd66c7c713dbf41ed5714015
Hack Reactor Students <accounts@hackreactor.com>
2015-07-11 11:28:55 -0700

    Add basic vector math and simulation code

--- client/watchout.js
+++ client/watchout.js
@@ -13,45 +13,87 @@ var options = {
 document.getElementsByClassName('game')[0].style.width = options.width;
 document.getElementsByClassName('game')[0].style.height = options.height;
 
-var game = d3.select('svg');
+var Vector = function(x, y) {
+  this.x = x;
+  this.y = y;
+};
 
-var updateEnemyPosition = function(positions) {
+Vector.prototype.dot = function(vec) {
+  return this.x * vec.x + this.y * vec.y;
+};
+
+Vector.prototype.length = function() {
+  return Math.sqrt(this.dot(this));
+};
+
+Vector.prototype.add = function(vec) {
+  return new Vector( this.x + vec.x, this.y + vec.y);
+};
+
+Vector.prototype.normalize = function() {
+  var length = this.length();
+  return new Vector(this.x/length, this.y/length);
+};
+
+Vector.prototype.multiplyScalar = function(scalar) {
+  return new Vector(this.x * scalar, this.y * scalar);
+};
+
+var randomEnemies = function(n) {
+  var results = [];
+  for (var i = 0; i < n; i++) {
+    results.push({
+      position: new Vector(Math.random() * options.width, Math.random() * options.height),
+      direction: new Vector(1, 0),
+      image: 'shuriken.svg'
+    });
+  }
+  return results;
+};
+
+var moveEnemies = function(delta) {
+  var speed = 0.1;
+  speed *= delta;
+  enemies.forEach(function (enemy) {
+    var velocity = enemy.direction.normalize().multiplyScalar(speed); 
+    // console.log(velocity);
+    enemy.position = enemy.position.add(velocity);
+  });
+};
+
+var redraw = function(positions) {
   var enemies = game.selectAll('.enemy').data(positions);
-
-  enemies.transition().tween('side-effects', function() {
-    return checkCollision;
-  }).duration(500).attr('x', function(pos) {
-    return pos.x;
+    enemies.attr('x', function(pos) {
+    return pos.position.x;
   }).attr('y', function(pos) {
-    return pos.y;
+    return pos.position.y;
   });
 
-  var enemyGraphics = [{'xlink:href': 'asteroid.png', class: 'asteroid enemy', width: '40px', height: '40px'}, {'xlink:href': 'shuriken.svg', class: 'shuriken enemy', width: '50px', height: '50px'}];
-  var randEnemy = enemyGraphics[Math.floor(Math.random() * enemyGraphics.length)]
-
-  enemies.enter().append('image').attr(randEnemy).attr('x', function(pos) {
-    return pos.x;
-  }).attr('y', function(pos) {
-    return pos.y;
-  });
+  enemies.enter().append('image').attr({'xlink:href': 'shuriken.svg', class: 'shuriken enemy', width: '50px', height: '50px'})
+    .attr('x', function(pos) { return pos.position.x; })
+    .attr('y', function(pos) { return pos.position.y; })
+    .attr('xlink:href', function(pos) { return pos.image; });
 
   enemies.exit().remove();
 };
 
-var randomPositions = function(n) {
-  var results = [];
-  for (var i = 0; i < n; i++) {
-    results.push({x: Math.random() * options.width, y: Math.random() * options.height});
-  }
+var game = d3.select('svg');
+var numEnemies = 10;
+var enemies = randomEnemies(numEnemies);
 
-  return results;
-};
+redraw(enemies);
 
-var numEnemies = 3;
-updateEnemyPosition(randomPositions(numEnemies));
-setInterval(function() {
-  updateEnemyPosition(randomPositions(numEnemies));
-}, 1000);
+var lastTime = 0;
+
+d3.timer(function(now) {
+  var delta = now - lastTime;
+  lastTime = now;
+  // console.log(delta);
+
+  moveEnemies(delta);
+  redraw(enemies);
+  checkCollision();
+});
 
 game.on('mousemove', function() {
   // player move
@@ -59,7 +101,6 @@ game.on('mousemove', function() {
   var player = game.selectAll('.player').data([playerPos]);
   player.attr('cx', function(pos) { return pos[0]}).attr('cy', function(pos) { return pos[1] });
   player.enter().append('circle').attr({r: 10, fill: 'rgb(200, 200, 255)', class: 'player'});
-  checkCollision();
 });
 
 var colliding = false;
